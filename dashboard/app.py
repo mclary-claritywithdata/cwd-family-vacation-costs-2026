913429de56b5fc4ed9d5f01a68226e08aaad7e0a
mclary-claritywithdata <mclary@claritywithdata.com>
2026-07-22 11:18:05 -0500

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 #FILE PATHS
 
-BASE_DIR = Path.(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 
 budget_file = BASE_DIR / "vacation_budget_simulator.xlsx"
